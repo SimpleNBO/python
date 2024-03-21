66606d33203233c5953a025919070607b5d1f2a1
unknown <simplenbo@gmail.com>
2024-03-21 20:50:50 -0300

    added lambda function filter

--- lambda.py
+++ lambda.py
@@ -1,9 +1,24 @@
 people = [
-    {'name': 'Harry', 'house': 'Gryffindor'},
-    {'name': 'Cho', 'house': 'Ravenclaw'},
-    {'name': 'Draco', 'house': 'Slytherin'}
+    {
+        'name': 'Harry',
+        'house': 'Gryffindor'
+    },
+    {
+        'name': 'Cho',
+        'house': 'Ravenclaw'
+    },
+    {
+        'name': 'Draco',
+        'house': 'Slytherin'
+    }
 ]
 
 people.sort(key=lambda _: _['name'])
+
+print(people)
+
+people.sort(key=lambda _: _['house'])
+
+print('#' * 100)
 
 print(people)
